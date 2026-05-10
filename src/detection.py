5b7991c5130010779f97292ab14ea64425292c26
Natalia <nata18629@gmail.com>
2026-05-10 02:57:03 +0200

    Trained model

--- src/detection.py
+++ src/detection.py
@@ -2,7 +2,7 @@ import random
 
 import detectron2
 from detectron2 import model_zoo
-from detectron2.engine import DefaultPredictor
+from detectron2.engine import DefaultPredictor, DefaultTrainer
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
@@ -15,16 +15,49 @@ import numpy as np
 IMAGE_PATH = "zdjecia/kilka.jpg"
 OUTPUT_DIR = "pieces"
 
-image = cv2.imread(IMAGE_PATH)
+
+# register_coco_instances("puzzle_train", {}, "puzzle_database/puzzle.json", "puzzle_database")
+
 cfg = get_cfg()
 cfg.MODEL.DEVICE='cpu'
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.DATASETS.TRAIN = ("puzzle_train",)
+cfg.DATASETS.TEST = ()
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+cfg.SOLVER.IMS_PER_BATCH = 2  # This is the real "batch size" commonly known to deep learning people
+cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+cfg.SOLVER.STEPS = []        # do not decay learning rate
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
 
-register_coco_instances("puzzle_train", {}, "puzzle_database/puzzle.json", "puzzle_database")
-dataset_dicts = DatasetCatalog.get("puzzle_train")
+## train
+# os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+# trainer = DefaultTrainer(cfg) 
+# trainer.resume_or_load(resume=False)
+# trainer.train()
+
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
+predictor = DefaultPredictor(cfg)
+
 balloon_metadata = MetadataCatalog.get("puzzle_train")
-for d in random.sample(dataset_dicts, 1):
-    img = cv2.imread(d["file_name"])
-    visualizer = Visualizer(img[:, :, ::-1], metadata=balloon_metadata, scale=0.5)
-    out = visualizer.draw_dataset_dict(d)
-    cv2.imshow("aaa", out.get_image()[:, :, ::-1])
-    cv2.waitKey()
+img = cv2.imread(IMAGE_PATH)
+outputs = predictor(img)
+visualizer = Visualizer(img[:, :, ::-1], metadata=balloon_metadata, scale=0.5)
+out = visualizer.draw_instance_predictions(outputs["instances"].to("cpu"))
+cv2.imshow("aaa", out.get_image()[:, :, ::-1])
+cv2.waitKey()
+
+
+
+def show_test():
+    dataset_dicts = DatasetCatalog.get("puzzle_train")
+    balloon_metadata = MetadataCatalog.get("puzzle_train")
+    for d in random.sample(dataset_dicts, 1):
+        img = cv2.imread(d["file_name"])
+        visualizer = Visualizer(img[:, :, ::-1], metadata=balloon_metadata, scale=0.5)
+        out = visualizer.draw_dataset_dict(d)
+        cv2.imshow("aaa", out.get_image()[:, :, ::-1])
+        cv2.waitKey()
